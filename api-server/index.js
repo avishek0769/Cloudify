b8c2f6055858da43fa2e0f48ada518a4850f9e15
avishek0769 <avishekadhikary42@gmail.com>
2026-03-09 18:06:32 +0530

    Create /deploy controller - Starts building and uploads to S3, shares the link

---
+++ api-server/index.js
@@ -0,0 +1,63 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
+
+const app = express();
+const PORT = 6000;
+
+const ecsClient = new ECSClient({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
+    },
+});
+
+app.use(express.json());
+
+app.post("/deploy", async (req, res) => {
+    const { githubUrl, projectId, pathToPackageJson } = req.body;
+
+    const command = new RunTaskCommand({
+        cluster: process.env.ECS_CLUSTER_NAME,
+        taskDefinition: process.env.ECS_TASK_DEFINITION,
+        launchType: "FARGATE",
+        count: 1,
+        networkConfiguration: {
+            awsvpcConfiguration: {
+                assignPublicIp: "ENABLED",
+                subnets: [
+                    process.env.SUBNET_ID_1,
+                    process.env.SUBNET_ID_2,
+                    process.env.SUBNET_ID_3,
+                ],
+                // securityGroups: ['']
+            },
+        },
+        overrides: {
+            containerOverrides: [
+                {
+                    name: process.env.CONTAINER_NAME,
+                    environment: [
+                        { name: "GITHUB_REPO_URL", value: githubUrl },
+                        { name: "PROJECT_ID", value: projectId },
+                        { name: "PATH_", value: pathToPackageJson || "" },
+                    ],
+                },
+            ],
+        },
+    });
+
+    await ecsClient.send(command);
+
+    return res.json({
+        status: "queued",
+        data: {
+            projectId,
+            url: `http://${projectId}.localhost:6001`,
+        },
+    });
+});
+
+app.listen(6000, () => console.log(`API Server running on ${PORT}...`));
