069943133981546ca345202043dd6dd0243ad8e7
avishek0769 <avishekadhikary42@gmail.com>
2026-07-15 00:17:40 +0530

    Update the s3-proxy script so that it can serve the root index.html file in s3, when the user in the browser reloads in a different /path

--- s3-reverse-proxy/index.js
+++ s3-reverse-proxy/index.js
@@ -1,5 +1,6 @@
 import httpProxy from "http-proxy";
 import express from "express";
+import path from "path";
 import { prisma } from "../api-server/utils/prisma.js";
 
 const PORT = 6001;
@@ -23,13 +24,41 @@ app.use(async (req, res, next) => {
     }
 
     const resolveTo = `${BASE_PATH}/${project.id}`;
+    req.projectId = project.id;
 
     proxy.web(req, res, { target: resolveTo, changeOrigin: true });
 });
 
 proxy.on("proxyReq", (proxyReq, req, res) => {
-    if (req.url == "/") proxyReq.path += "index.html";
-    console.log(proxyReq.path);
+    const urlPath = req.url;
+    
+    const pathname = urlPath.split("?")[0];
+    const extension = path.extname(pathname);
+
+    let targetPath = "";
+
+    if (pathname === "/" || pathname === "") {
+        targetPath = `/vercel.output/__outputs/${req.projectId}/index.html`;
+    } else if (!extension) {
+        targetPath = `/vercel.output/__outputs/${req.projectId}/index.html`;
+    } else {
+        targetPath = `/vercel.output/__outputs/${req.projectId}${pathname}`;
+    }
+
+    const queryIndex = urlPath.indexOf("?");
+    if (queryIndex !== -1) {
+        targetPath += urlPath.substring(queryIndex);
+    }
+
+    proxyReq.path = targetPath;
+});
+
+proxy.on("error", (err, req, res) => {
+    console.error("Proxy Error:", err);
+    if (!res.headersSent) {
+        res.writeHead(502, { "Content-Type": "application/json" });
+    }
+    res.end(JSON.stringify({ error: "Proxy connection failed." }));
 });
 
 app.listen(PORT, () => console.log(`Proxy server running on ${PORT}...`));
